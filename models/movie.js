60397a5215d9b638c926cfb7682e5c82addd1f0e
hayjmuir <haymuir@gmail.com>
2020-05-17 21:16:26 -0700

    updated movie.js

--- models/movie.js
+++ models/movie.js
@@ -1,36 +1,15 @@
-"use strict";
+module.exports = function (sequelize, DataTypes) {
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
+  let Movie = sequelize.define("movie", {
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+    movie_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    watched: {
+      type: DataTypes.BOOLEAN
+    }
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-  })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
   });
-
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+  return Movie;
+};
